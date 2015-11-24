1d2e9fc1d8661b94b258b9b970e764a2fba74d93
David Greenberg <dgreenberg@grefin.com>
2015-11-24 09:22:07 -0800

    Removed Files
    Removed Files and Added a Visual Element

--- BackPropogation/BackPropogation/ReccurentLoops.cu
+++ BackPropogation/BackPropogation/ReccurentLoops.cu
@@ -379,7 +379,7 @@ void ReccurentLoops::testTraining(){
 			exit(0);
 		}
 		//this->mainNetwork->removeNeuron(1, 0);
-		this->mainNetwork->addWeight(1);
+		//this->mainNetwork->addWeight(1);
 		cout << "Training Start" << endl;
 		
 		for (int loops = 0; loops < this->settings.i_numberTimesThroughFile; loops++){
